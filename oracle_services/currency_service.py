9487ccc6c54f4094cf43f2c4a02f14453a34e8ae
gowrivs <>
2018-10-07 07:16:23 -0700

    new currencies added

--- oracle_services/currency_service.py
+++ oracle_services/currency_service.py
@@ -31,6 +31,22 @@ r = requests.get('https://api.coinmarketcap.com/v2/ticker/?convert=CNY')
 r_eth = r.json()
 # print("1 ETH in CNY is " +str(r_eth['data']['1027']['quotes']['CNY']['price']))
 currency_dict["CNY"]=1/r_eth['data']['1027']['quotes']['CNY']['price']
+#ETH-BTC
+r = requests.get('https://api.coinmarketcap.com/v2/ticker/?convert=BTC')
+r_eth = r.json()
+# print("1 ETH in BTC is " +str(r_eth['data']['1027']['quotes']['BTC']['price']))
+currency_dict["BTC"]=r_eth['data']['1027']['quotes']['BTC']['price']
+#ETH-XRP
+r = requests.get('https://api.coinmarketcap.com/v2/ticker/?convert=XRP')
+r_eth = r.json()
+# print("1 ETH in XRP is " +str(r_eth['data']['1027']['quotes']['XRP']['price']))
+currency_dict["XRP"]=r_eth['data']['1027']['quotes']['XRP']['price']
+#ETH-LTC
+r = requests.get('https://api.coinmarketcap.com/v2/ticker/?convert=LTC')
+r_eth = r.json()
+# print("1 ETH in LTC is " +str(r_eth['data']['1027']['quotes']['LTC']['price']))
+currency_dict["LTC"]=r_eth['data']['1027']['quotes']['LTC']['price']
+
 
 for k,v in currency_dict.items():
             print(k, v)
